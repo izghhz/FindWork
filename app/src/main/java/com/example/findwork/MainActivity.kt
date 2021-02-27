34abb0513b25e0feb29f42fb35f6d68e2d7493e0
izghhz <61828891+izghhz@users.noreply.github.com>
2021-02-27 16:58:47 +0800

    调整首页布局

--- app/src/main/java/com/example/findwork/MainActivity.kt
+++ app/src/main/java/com/example/findwork/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.findwork
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -8,11 +11,13 @@ import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
 import com.example.common.BaseActivity
 import com.example.video.ui.FooFragment
+import com.hoko.blur.HokoBlur
 import kotlinx.android.synthetic.main.about_icon_layout.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.gallery_icon_layout.*
 import kotlinx.android.synthetic.main.home_icon_layout.*
 import kotlinx.android.synthetic.main.video_icon_layout.*
+
 
 @Route(path = "/home/main")
 class MainActivity : BaseActivity() {
@@ -26,6 +31,19 @@ class MainActivity : BaseActivity() {
             2 to galleryMotionLayout,
             3 to aboutMotionLayout
         )
+        /*val bitmap = BitmapFactory.decodeResource(resources, R.drawable.bakground)
+        val outBitmap: Bitmap = HokoBlur.with(this).radius(25).blur(bitmap)
+        linearLayout.background = BitmapDrawable(outBitmap)*/
+        linearLayout.setBackgroundResource(R.drawable.bakground)
+/*        Blurry.with(context)
+            .radius(10)//模糊半径
+            .sampling(8)//缩放大小，先缩小再放大
+            .color(Color.argb(66, 255, 255, 0))//颜色
+            .async()//是否异步
+            .animate(500)//显示动画，目前仅支持淡入淡出，默认时间是300毫秒，仅支持传入控件为ViewGroup
+            .onto(linearLayout)*/
+
+
         homeMotionLayout.apply {
             progress = 0f//设置动画进度，整个过程为0-1
             //transitionToEnd()//播放动画
@@ -79,7 +97,8 @@ class MainActivity : BaseActivity() {
                 }
             }
             setCurrentItem(App.nowTab, false)//确保nowTab与当前页面序号一致
-            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {//页面发生变化时的监听
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                //页面发生变化时的监听
                 override fun onPageSelected(position: Int) {
                     map.values.forEach { it.transitionToStart() }//先全部回到初始状态
                     map[position]?.transitionToEnd()//当前页面对应按钮的进入结束状态
--- network/src/main/java/com/example/WebActivity.kt
+++ network/src/main/java/com/example/WebActivity.kt
@@ -2,8 +2,11 @@ package com.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.alibaba.android.arouter.launcher.ARouter
@@ -24,6 +27,14 @@ class WebActivity : AppCompatActivity() {
             settings.domStorageEnabled = true
             settings.loadsImagesAutomatically= true
             webViewClient= WebViewClient()
+            webChromeClient = object :WebChromeClient(){
+                override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                    super.onProgressChanged(view, newProgress)
+                    if (newProgress == 100) this@WebActivity.findViewById<ProgressBar>(R.id.progressBar).visibility = View.INVISIBLE
+                }
+            }
+
+
 
           loadUrl(urlKey)
 
